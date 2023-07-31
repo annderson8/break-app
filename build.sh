de10ec78bd2e7f4f8abeaaee2911bd456941c57d
Annderson Rey <anndersonrey@gamil.com>
2023-07-31 10:08:45 +1000

    fixing build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Exit on error
-set -o errexit
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-pip install -r requirements.txt
+# echo "Make Migration..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
 
-python manage.py collectstatic --no-input
-
-# python manage.py migrate
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
